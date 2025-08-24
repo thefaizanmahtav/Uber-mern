c9c401e72971fed5a2bd0dac8e01175bcd71371a
Faizan Ahmad <faizankhan56752@gmail.com>
2025-08-25 04:14:48 +0530

    footer

--- Frontend/src/pages/Footer/Footer.tsx
+++ Frontend/src/pages/Footer/Footer.tsx
@@ -100,6 +100,7 @@ function Footer() {
     return (
         <footer className="flex py-10 flex-col md:p-[60px] bg-black xl:px-[148px] md:px-10 px-6">
 
+            {/* home page link */}
             <div className="my-8">
                 <Link
                     className="text-white font-semibold text-2xl"
@@ -107,6 +108,7 @@ function Footer() {
                     Uber
                 </Link>
             </div>
+
 
             <div className="mb-12">
                 <Link
@@ -116,132 +118,157 @@ function Footer() {
                 </Link>
             </div>
 
-            {/* company */}
+            {/* wrapper company, prodects wrapper, Global citizenship & Travel lg size*/}
 
-            <div className="w-fit">
+            <div className="lg:flex lg:gap-15">
 
-                <h1 className="text-white text-xl font-semibold mb-5">
-                    Company
-                </h1>
-            </div>
+                {/* wrapper company & prodects md size */}
 
-            <div className="flex flex-col gap-4 mb-18">
+                <div className="md:flex justify-between gap-10">
 
-                {companyLink.map(({ lable, to }) => (
-                    <Link
-                        key={lable}
-                        className="text-white text-[15px]"
-                        to={to}>
-                        {lable}
-                    </Link>
-                ))}
-            </div>
+                    {/* company */}
 
+                    <div className="w-fit md:w-[300px]">
 
-            {/* Products */}
+                        <h1 className="text-white lg:w-full text-xl font-semibold mb-5">
+                            Company
+                        </h1>
 
-            <div className="w-fit">
+                        <div className="flex flex-col gap-4 mb-18">
 
-                <h1 className="text-white text-xl font-semibold mb-5">
-                    Products
-                </h1>
-            </div>
-
-            <div className="flex flex-col gap-4 mb-18">
-
-                {ProductsLink.map(({ lable, to }) => (
-                    <Link
-                        key={lable}
-                        className="text-white text-[15px]"
-                        to={to}>
-                        {lable}
-                    </Link>
-                ))}
-            </div>
-
-            {/* Global citizenship */}
-
-            <div className="w-fit">
-
-                <h1 className="text-white text-xl font-semibold mb-5">
-                    Global citizenship
-                </h1>
-            </div>
-
-            <div className="flex flex-col gap-4 mb-18">
-
-                {GlobalCitizenshipLink.map(({ lable, to }) => (
-                    <Link
-                        key={lable}
-                        className="text-white text-[15px]"
-                        to={to}>
-                        {lable}
-                    </Link>
-                ))}
-            </div>
-
-            {/* Travel */}
-
-            <div className="w-fit">
-
-                <h1 className="text-white text-xl font-semibold mb-5">
-                    Travel
-                </h1>
-            </div>
-
-            <div className="flex flex-col gap-4 mb-20">
-
-                {TravelLink.map(({ lable, to }) => (
-                    <Link
-                        key={lable}
-                        className="text-white text-[15px]"
-                        to={to}>
-                        {lable}
-                    </Link>
-                ))}
-            </div>
-
-            {/* Icons */}
-
-            <div className="flex items-center w-full gap-10 mb-20">
-
-                {icons.map(({ icon, to }) => (
-                    <div className="p-2 bg-black rounded-xl hover:bg-white/10">
-                        <Link
-                            key={to}
-                            className="flex items-center justify-center h-10 w-10"
-                            to={to}>
-                            {icon}
-                        </Link>
+                            {companyLink.map(({ lable, to }) => (
+                                <Link
+                                    key={lable}
+                                    className="text-white text-[15px]"
+                                    to={to}>
+                                    {lable}
+                                </Link>
+                            ))}
+                        </div>
                     </div>
-                ))}
-            </div>
 
-            {/* languange item */}
 
-            <div className="flex w-fit flex-col mb-10">
+                    {/* Products */}
 
-                <div className="flex items-center gap-2 group cursor-pointer rounded-xl px-4 py-2 transition-colors hover:bg-white/10">
-                    <svg
-                        width="19" height="19" viewBox="0 0 24 24" fill="none"><title>Globe</title><path d="M12 1C5.9 1 1 5.9 1 12s4.9 11 11 11 11-4.9 11-11S18.1 1 12 1Zm8 11c0 .7-.1 1.4-.3 2-.6-1.5-1.6-3.1-3-4.7l1.8-1.8c1 1.3 1.5 2.8 1.5 4.5ZM6.5 6.5c1.3 0 3.6.8 6 2.9l-3.2 3.2C7.1 9.8 6.5 7.5 6.5 6.5Zm8.1 5c2.3 2.7 2.9 5 2.9 6-1.3 0-3.6-.8-6-2.9l3.1-3.1Zm1.9-6.1-1.9 1.9c-1.6-1.4-3.2-2.4-4.7-3 .7-.2 1.3-.3 2-.3 1.8 0 3.3.5 4.6 1.4ZM4 12c0-.7.1-1.4.3-2 .6 1.5 1.6 3.1 3 4.7l-1.8 1.8C4.5 15.2 4 13.7 4 12Zm3.5 6.6 1.9-1.9c1.6 1.4 3.2 2.4 4.7 3-.7.2-1.3.3-2 .3-1.8 0-3.3-.5-4.6-1.4Z" fill="white"></path>
-                    </svg>
-                    <span className="text-[15px] text-white font-semibold">English</span>
+                    <div className="w-fit md:w-[300px]">
+
+                        <h1 className="text-white text-xl font-semibold mb-5">
+                            Products
+                        </h1>
+
+                        <div className="flex flex-col gap-4 mb-18">
+
+                            {ProductsLink.map(({ lable, to }) => (
+                                <Link
+                                    key={lable}
+                                    className="text-white text-[15px]"
+                                    to={to}>
+                                    {lable}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+
                 </div>
 
-                {/* location item */}
+                {/* wrapper Global citizenship & Travel md size */}
 
-                <div className="flex items-center gap-2 group cursor-pointer rounded-xl px-4 py-2 transition-colors hover:bg-white/10">
-                    <svg
-                        width="19" height="19" viewBox="0 0 24 24" fill="none"><title>Location marker</title><path d="M18.7 3.8C15 .1 9 .1 5.3 3.8c-3.7 3.7-3.7 9.8 0 13.5L12 24l6.7-6.8c3.7-3.6 3.7-9.7 0-13.4ZM12 12.5c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2Z" fill="white"></path>
-                    </svg>
-                    <span className="text-[15px] text-white font-semibold">Lucknow</span>
+                <div className="md:flex justify-between gap-10">
+
+                    {/* Global citizenship */}
+
+                    <div className="w-fit md:w-[300px]">
+
+                        <h1 className="text-white text-xl font-semibold mb-5">
+                            Global citizenship
+                        </h1>
+
+                        <div className="flex flex-col gap-4 mb-18">
+
+                            {GlobalCitizenshipLink.map(({ lable, to }) => (
+                                <Link
+                                    key={lable}
+                                    className="text-white text-[15px]"
+                                    to={to}>
+                                    {lable}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+
+
+                    {/* Travel */}
+
+                    <div className="w-fit md:w-[300px]">
+
+                        <h1 className="text-white text-xl font-semibold mb-5">
+                            Travel
+                        </h1>
+
+                        <div className="flex flex-col gap-4 mb-20">
+
+                            {TravelLink.map(({ lable, to }) => (
+                                <Link
+                                    key={lable}
+                                    className="text-white text-[15px]"
+                                    to={to}>
+                                    {lable}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            {/* wrapper Icons & language item */}
+
+            <div className="lg:flex lg:items-center">
+
+                {/* Icons */}
+
+                <div className="flex items-center w-full gap-10 mb-20 ">
+
+                    {icons.map(({ icon, to }) => (
+                        <div className="p-2 bg-black rounded-xl hover:bg-white/10">
+                            <Link
+                                key={to}
+                                className="flex items-center justify-center h-10 w-10"
+                                to={to}>
+                                {icon}
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+
+                {/* languange item */}
+
+                <div className="flex w-fit max-md:flex-col lg:gap-5 mb-10 lg:mb-20">
+
+                    <div className="flex items-center gap-2 group cursor-pointer rounded-xl px-4 py-2 transition-colors hover:bg-white/10">
+                        <svg
+                            width="19" height="19" viewBox="0 0 24 24" fill="none"><title>Globe</title><path d="M12 1C5.9 1 1 5.9 1 12s4.9 11 11 11 11-4.9 11-11S18.1 1 12 1Zm8 11c0 .7-.1 1.4-.3 2-.6-1.5-1.6-3.1-3-4.7l1.8-1.8c1 1.3 1.5 2.8 1.5 4.5ZM6.5 6.5c1.3 0 3.6.8 6 2.9l-3.2 3.2C7.1 9.8 6.5 7.5 6.5 6.5Zm8.1 5c2.3 2.7 2.9 5 2.9 6-1.3 0-3.6-.8-6-2.9l3.1-3.1Zm1.9-6.1-1.9 1.9c-1.6-1.4-3.2-2.4-4.7-3 .7-.2 1.3-.3 2-.3 1.8 0 3.3.5 4.6 1.4ZM4 12c0-.7.1-1.4.3-2 .6 1.5 1.6 3.1 3 4.7l-1.8 1.8C4.5 15.2 4 13.7 4 12Zm3.5 6.6 1.9-1.9c1.6 1.4 3.2 2.4 4.7 3-.7.2-1.3.3-2 .3-1.8 0-3.3-.5-4.6-1.4Z" fill="white"></path>
+                        </svg>
+                        <span className="text-[15px] text-white font-semibold">English</span>
+                    </div>
+
+                    {/* location item */}
+
+                    <div className="flex items-center gap-2 group cursor-pointer rounded-xl px-4 py-2 transition-colors hover:bg-white/10">
+                        <svg
+                            width="19" height="19" viewBox="0 0 24 24" fill="none"><title>Location marker</title><path d="M18.7 3.8C15 .1 9 .1 5.3 3.8c-3.7 3.7-3.7 9.8 0 13.5L12 24l6.7-6.8c3.7-3.6 3.7-9.7 0-13.4ZM12 12.5c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2Z" fill="white"></path>
+                        </svg>
+                        <span className="text-[15px] text-white font-semibold">Lucknow</span>
+                    </div>
+
                 </div>
 
             </div>
 
             {/*app downloading link */}
 
-            <div className="flex items-center w-fit h-fit gap-2">
+            <div className="flex items-center w-fit h-fit gap-2 mb-3">
 
                 <div className="w-[150px] h-[100px]">
                     <Link
@@ -262,44 +289,52 @@ function Footer() {
                 </div>
             </div>
 
-            {/* copy right  */}
 
-            <div className="flex gap-1 mb-24">
+            {/* wrapper copyright & terms */}
 
-                <span className="text-white/80 text-sm">&copy;</span>
+            <div className="md:flex md:h-fit md:justify-between">
 
-                <p className="text-white/80 text-sm">
-                    2025 Thefaizanmahtav Technologies Inc.
-                </p>
 
+                {/* copy right  */}
+
+                <div className="flex gap-1 mb-24 md:mb-5">
+
+                    <span className="text-white/80 text-sm">&copy;</span>
+
+                    <p className="text-white/80 text-sm">
+                        2025 Thefaizanmahtav Technologies Inc.
+                    </p>
+
+                </div>
+
+                {/* terms */}
+
+                <div className="flex gap-20 mb-6 md:mb-5">
+
+                    <Link
+                        to={''}
+                        className="text-white/80 text-sm"
+                    >
+                        Privacy
+                    </Link>
+
+                    <Link
+                        to={''}
+                        className="text-white/80 text-sm"
+                    >
+                        Accessibility
+                    </Link>
+
+                    <Link
+                        to={''}
+                        className="text-white/80 text-sm"
+                    >
+                        Terms
+                    </Link>
+
+                </div>
             </div>
 
-            {/* terms */}
-
-            <div className="flex gap-20 mb-6">
-
-                <Link
-                    to={''}
-                    className="text-white/80 text-sm"
-                >
-                    Privacy
-                </Link>
-
-                <Link
-                    to={''}
-                    className="text-white/80 text-sm"
-                >
-                    Accessibility
-                </Link>
-
-                <Link
-                    to={''}
-                    className="text-white/80 text-sm"
-                >
-                    Terms
-                </Link>
-
-            </div>
 
         </footer >
     )
